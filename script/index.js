542effd1c825100afe91ec26480a6913fffbda06
nurislam243 <mdnurislam@gmail.com>
2025-03-13 13:04:48 +0600

    added dynamic catagory wise video

--- script/index.js
+++ script/index.js
@@ -10,6 +10,17 @@ function loadVideos(){
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoryVideos=(id)=>{
+    
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`
+
+    console.log(url);
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data => displayVideos(data.category))
+}
+
 function displayCategories(categories){
     const categoryContainer = document.getElementById("category-container");
 
@@ -18,7 +29,7 @@ function displayCategories(categories){
         const categoryDiv = document.createElement("div");
 
         categoryDiv.innerHTML = `
-    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
     categoryContainer.appendChild(categoryDiv)
     }
@@ -27,26 +38,38 @@ function displayCategories(categories){
 const displayVideos=(videos)=>{
     const videoContainer = document.getElementById("video-container");
 
+    videoContainer.innerHTML="";
+
     videos.forEach(video => {
         const videoCard = document.createElement("div");
 
         videoCard.innerHTML = `
-        
-        <div class="card bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+        <div class="card bg-base-100">
+            <figure class="relative">
+              <img class="w-full h-[150px] object-cover"
+                src="${video.thumbnail}"
+                alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-white text-sm rounded bg-black px-2">3hrs 56 min ago</span>
+            </figure>
+            
+            <div class="flex gap-3 px-0 py-5">
+              <div class="profile">
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                      <img src="${video.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+              </div>
 
+              <div class="intro">
+                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}
+                     <img class="w-5 h-5" src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">
+                </p>
+                <p class="text-sm text-gray-400 flex">${video.others.views}views</p>
+              </div>
+            </div>
+        </div>
         `;
         videoContainer.appendChild(videoCard);
     });
@@ -54,4 +77,3 @@ const displayVideos=(videos)=>{
 }
 
 loadCategories();
-loadVideos();
